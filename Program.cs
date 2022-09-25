134fae4f333f55ecccd11f2de8f2044c579d3a9d
inanakalin <111204187+inanakalin@users.noreply.github.com>
2022-09-25 14:59:53 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        inan(); yaş(); akalın();
+        
+        
+    }
+
+    public static void inan()
+    {
+        System.Console.WriteLine("İnan ");
+    }
+
+        public static void akalın()
+    {
+        System.Console.WriteLine("Akalın");
+    }
+
+        public static void yaş()
+    {
+        System.Console.WriteLine(35);
+    }
+
+}
+    
